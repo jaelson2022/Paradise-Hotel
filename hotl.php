8de299bc81332a613e90f2325e1634e251c74d31
jaelson2022 <jaelson.jaelsonsantos.freitas@gmail.com>
2023-05-14 01:00:09 -0300

    Primeira Atualização No Site

--- hotl.php
+++ hotl.php
@@ -1,3 +1,39 @@
 <?php
-echo "Hotel";
+
 ?>
+<!Doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-sCale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
+    <title>Homo Paradise Hotel</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <a href="#">Logo</a>
+            <nav class="nav">
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>Facilidades</li>
+                    <li><a href="#"></a>Quartos</li>
+                    <li><a href="#"></a>Contatos</li>
+                </ul>
+                   
+            </nav>
+        </header>
+        <div class="hero">
+            <main class="main">
+                <div class="center">
+                    <h1>Paradise Hotel</h1>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>    
+</html>
